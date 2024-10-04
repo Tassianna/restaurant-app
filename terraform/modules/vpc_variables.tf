596910ca84d8efc380c7450f69f36e9589716fdb
Grace Purcell <gracecpurcell2695@gmail.com>
2024-10-04 11:52:44 +0200

    moved variables to main file

--- terraform/modules/vpc_variables.tf
+++ terraform/modules/vpc_variables.tf
@@ -1,20 +1,20 @@
-variable "cidr_block" {
-    type = string
-}
+# variable "cidr_block" {
+#     type = string
+# }
 
-variable "vpc_name"{
-    type = string
-}
+# variable "vpc_name"{
+#     type = string
+# }
 
-variable "subnets"{
-    type = map(object({
-    cidr_block = string
+# variable "subnets"{
+#     type = map(object({
+#     cidr_block = string
 
-    map_public_ip_on_launch = bool
+#     map_public_ip_on_launch = bool
 
-    availability_zone = string
-  }))
-}
+#     availability_zone = string
+#   }))
+# }
 
 ###################this is how the subnets should look like:#########################
 ###subnets = {
