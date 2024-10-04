cf45eda1bd7a7ee92164edc63ab5dcdbcc64f08a
Grace Purcell <gracecpurcell2695@gmail.com>
2024-10-04 15:11:31 +0200

    Updated Readme, assets screenshots, fixed secruitygoup ref

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "instance" {
   instance_type               = var.instance_type
   key_name                    = var.key_name
   availability_zone           = each.value.availability_zone
-  vpc_security_group_ids      = [each.value.vpc_security_group == true ? aws_security_group.public_security_group.id : aws_security_group.private_security_group.id]
+  vpc_security_group_ids      = [each.value.vpc_security_group == true ? aws_security_group.security_group["public-security-group"].id : aws_security_group.security_group["private-security-group"].id]
   subnet_id                   = aws_subnet.subnet[each.value.subnet].id
   associate_public_ip_address = each.value.associate_public_ip_address
 
