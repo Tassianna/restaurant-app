379d534c48718842a3f12bffa36f9ba0a4c674a9
Grace Purcell <gracecpurcell2695@gmail.com>
2024-10-04 12:43:58 +0200

    route tables and associations

--- terraform/natgateway.tf
+++ terraform/natgateway.tf
@@ -1,6 +1,6 @@
 resource "aws_nat_gateway" "nazgw" {
   allocation_id = aws_eip.eip.id
-  subnet_id = aws_subnet.subnet["public_subnet"].id
+  subnet_id     = aws_subnet.subnet["public_subnet"].id
   tags = {
     Name = "natgw"
   }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,15 +1,15 @@
 # definition of variables to be used in resources and modules
 
 variable "cidr_block" {
-    type = string
+  type = string
 }
 
-variable "vpc_name"{
-    type = string
+variable "vpc_name" {
+  type = string
 }
 
-variable "subnets"{
-    type = map(object({
+variable "subnets" {
+  type = map(object({
     cidr_block = string
 
     map_public_ip_on_launch = bool
@@ -19,18 +19,31 @@ variable "subnets"{
 }
 
 variable "security_groups" {
-    type = map(object({
-        desc = string
-        ingress = map(object({
-            from_port = number
-            to_port = number
-            protocol = string
-            cidr_blocks = bool
-        }))
-        egress = map(object({
-            from_port = number
-            to_port = number
-            protocol = string
-        }))
+  type = map(object({
+    desc = string
+    ingress = map(object({
+      from_port   = number
+      to_port     = number
+      protocol    = string
+      cidr_blocks = bool
     }))
+    egress = map(object({
+      from_port = number
+      to_port   = number
+      protocol  = string
+    }))
+  }))
+}
+
+variable "route-associations" {
+  type = map(object({
+    subnet_id      = string
+    route_table_id = string
+  }))
+}
+variable "route-tables" {
+  type = map(object({
+    cidr_block = string
+    gateway_id = string
+  }))
 }
