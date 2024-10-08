da8bf473a860d86f8a713ba8320a56f0c88e2cc6
tassianna <tasi.dafn@gmail.com>
2024-10-08 11:36:29 +0200

    transfer cloud_watch resources into the new scaling-terraform folder

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -59,22 +59,3 @@ variable "key_name" {
   type = string
 }
 
-
-#################aws_cloudwatch_metric_alarm###############
-
-variable "alarms"{
-    type= map (object({
-      alarm_name          = string  
-      comparison_operator = string  
-      evaluation_periods  = string  
-      metric_name         = string  
-      namespace           = string  
-      period              = string  
-      statistic           = string  
-      threshold           = string  
-      alarm_description   = string   
-      alarm_actions       = string   
-      dimensions          = string
-    }))
-}
-
