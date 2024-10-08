6bb6c45410b89778655cc8e9b031236bc454d70a
tassianna <tasi.dafn@gmail.com>
2024-10-08 11:19:58 +0200

    cloudwatch alarms for all autoscaling groups

---
+++ terraform/cloud_watch.tf
@@ -0,0 +1,23 @@
+
+
+resource "aws_cloudwatch_metric_alarm" "alarms" {
+    alarm_name          = each.value.alarm_name
+    comparison_operator = each.value.comparison_operator
+    evaluation_periods  = each.value.evaluation_periods
+    metric_name         = each.value.metric_name
+    namespace           = each.value.namespace
+    period              = each.value.period
+    statistic           = each.value.statistic
+    threshold           = each.value.threshold
+    alarm_description   = each.value.alarm_description
+    alarm_actions       = [aws_autoscaling_policy.scale_up.arn]                  #TODO: change name
+    dimensions = {
+        AutoScalingGroupName = aws_autoscaling_group.backend_service_asg.name    #TODO: change names
+    }
+    for_each = vars.alarms
+}
+
+
+#resource "aws_cloudwatch_log_group" "app_log_group" {
+#  name = "/aws/ec2/backend-app-logs"
+#}
