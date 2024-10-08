f0ad834587525e80961d23622c232b2077a81738
Grace Purcell <gracecpurcell2695@gmail.com>
2024-10-08 22:19:04 +0200

    user data to launch docker

--- scaling-terraform/launch-template.tf
+++ scaling-terraform/launch-template.tf
@@ -14,4 +14,10 @@ resource "aws_launch_template" "launch_template" {
   instance_type = "t2.micro"
   image_id      = each.value.id # <-- this needs to be snapshot image 
   key_name      = var.key_name
+  
+  user_data = <<-EOF
+        #!/bin/bash
+        cd ./restaurant-app
+        docker compose up -d
+        EOF
 }
