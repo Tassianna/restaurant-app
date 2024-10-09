1a954cfb4397073de8dd5cc5d5ccee096ea1fecc
Grace Purcell <gracecpurcell2695@gmail.com>
2024-10-09 12:55:36 +0200

    clean cloud init for custom ami

--- automate_destroy.sh
+++ automate_destroy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ./scaling-terraform
-terraform destroy -var="private_security_group_id=sg-0e48bd21890d87039" -var="private_subnet_id=subnet-0304b8ffc17063fbe"
+terraform destroy -var="private_security_group_id=sg-0ff3677c8614843c7" -var="private_subnet_id=subnet-02540f85aebec193e"
 cd ..
 cd ./terraform
 terraform destroy --auto-approve
