2023e3ad5a7637d5738605fa709a84b4ce50d82a
Grace Purcell <gracecpurcell2695@gmail.com>
2024-10-09 11:57:34 +0200

    updated script

--- scaling-terraform/template-startup/start-up.sh
+++ scaling-terraform/template-startup/start-up.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
+echo "start"
+# Wait for Docker service to be fully up
+while ! systemctl is-active --quiet docker; do
+    echo "Waiting for Docker to start..."
+    sleep 5
+done
 
-exec > /var/log/user-data.log 2>&1  # Log output to a file
+cd ~/restaurant-app
+sudo docker compose up
 
-if [ ! -f /var/lib/cloud/instance/boot-finished ]; then
-    echo "User data script started."
-
-    # Wait for Docker service to be fully up
-    while ! systemctl is-active --quiet docker; do
-        echo "Waiting for Docker to start..."
-        sleep 5
-    done
-    
-    cd ./restaurant-app
-    sudo docker compose up
-    
-    echo "Docker containers started."
-fi
+echo "Docker containers started."
