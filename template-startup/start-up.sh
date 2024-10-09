78858d6cb2b452de8da3834cb8875fc65a1c8f9c
Grace Purcell <gracecpurcell2695@gmail.com>
2024-10-09 10:22:38 +0200

    updated script and template

--- template-startup/start-up.sh
+++ template-startup/start-up.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+
+exec > /var/log/user-data.log 2>&1  # Log output to a file
+
 if [ ! -f /var/lib/cloud/instance/boot-finished ]; then
-    # Your setup script here
-    cd /path/to/restaurant-app
-    sudo docker compose up -d
+    echo "User data script started."
+
+    # Wait for Docker service to be fully up
+    while ! systemctl is-active --quiet docker; do
+        echo "Waiting for Docker to start..."
+        sleep 5
+    done
+    
+    cd ./restaurant-app
+    sudo docker compose up
+    
+    echo "Docker containers started."
 fi
